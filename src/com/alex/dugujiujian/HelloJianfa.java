697141c5d6767a10975fadba67dda2ee843b72ea
alex mk <alexmk@aliyun.com>
2021-02-24 14:33:12 +0800

    hello world BBBBB

--- src/com/alex/dugujiujian/HelloJianfa.java
+++ src/com/alex/dugujiujian/HelloJianfa.java
@@ -2,5 +2,10 @@ package com.alex.dugujiujian;
 public class HelloJianfa {
 	public static void main(String[] args) {
 		System.out.println("Hello dugujiujian");
+		System.out.println("Hello dugujiujian - BBBBB");
+		System.out.println("Hello dugujiujian - BBBBB");
+		System.out.println("Hello dugujiujian - BBBBB");
+		System.out.println("Hello dugujiujian - BBBBB");
+		System.out.println("Hello dugujiujian - BBBBB");
 	}
 }
